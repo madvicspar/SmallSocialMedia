87658102ae22fcb983d19a6fddeada694f2868fa
madvicspar <vmakkuzina@edu.hse.ru>
2024-07-23 00:01:06 +0500

    Added news feed template

---
+++ SimpleSocialMedia/Controllers/FeedController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleSocialMedia.Utilities.Data;
+
+namespace SimpleSocialMedia.Controllers
+{
+    public class FeedController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FeedController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Feed
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _context.Posts
+                .Include(p => p.User)
+                .OrderByDescending(p => p.CreatedAt)
+                .ToListAsync();
+
+            return View(posts);
+        }
+    }
+
+}
