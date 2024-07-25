6ca6b31e05a9610aab393311a37998bbfa90f77d
madvicspar <vmakkuzina@edu.hse.ru>
2024-07-26 04:15:46 +0500

    Layout of a modular window for editing profile data has been created

---
+++ SimpleSocialMedia/wwwroot/js/profile.js
@@ -0,0 +1,17 @@
+﻿var modal = $('#editProfileModal');
+var btn = $('.edit-profile-button');
+var span = $('.close');
+
+btn.on('click', function () {
+    modal.show();
+});
+
+span.on('click', function () {
+    modal.hide();
+});
+
+$(window).on('click', function (event) {
+    if ($(event.target).is(modal)) {
+        modal.hide();
+    }
+});
