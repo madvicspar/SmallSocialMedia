a3365574fff7f6d0ac173f0e365fdb5807a19133
madvicspar <vmakkuzina@edu.hse.ru>
2024-07-26 16:28:21 +0500

    Implemented the option to select a photo for the profile header when editing a profile

--- SimpleSocialMedia/wwwroot/js/profile.js
+++ SimpleSocialMedia/wwwroot/js/profile.js
@@ -15,3 +15,23 @@ $(window).on('click', function (event) {
         modal.hide();
     }
 });
+
+$('.add-header-photo-container').on('click', function () {
+    $('#add-header-photo-input').click();
+});
+
+$('#add-header-photo-input').on('change', function () {
+    var input = this;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('.edit-header-photo').css('background-image', 'url(' + e.target.result + ')');
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+});
+
+$('.delete-header-photo-container').on('click', function () {
+    var backgroundImage = 'url(' + initialHeaderPhotoUrl + '), url(' + defaultHeaderPhotoUrl + ')';
+    $('.edit-header-photo').css('background-image', backgroundImage);
+});
