0916de9ecab5bcb81e3b59e6649f5d30a9d05e2f
madvicspar <vmakkuzina@edu.hse.ru>
2024-08-01 00:38:40 +0500

    The display of posts of tracked users is implemented, as well as popular news

--- SimpleSocialMedia/Controllers/FeedController.cs
+++ SimpleSocialMedia/Controllers/FeedController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using SimpleSocialMedia.Models;
 using SimpleSocialMedia.Utilities.Data;
+using System.Linq;
+using System.Security.Claims;
 
 namespace SimpleSocialMedia.Controllers
 {
@@ -12,20 +15,63 @@ namespace SimpleSocialMedia.Controllers
         // GET: Feed
         public async Task<IActionResult> Index()
         {
+            return View(await GetMyNews());
+        }
+
+        public async Task<IActionResult> GetMyNewsTab()
+        {
+            return PartialView("_PostsListPartial", await GetMyNews());
+        }
+
+        public async Task<IActionResult> GetPopularPostsTab()
+        {
+            return PartialView("_PostsListPartial", await GetPopularPosts());
+        }
+
+        public async Task<List<PostModel>> GetMyNews()
+        {
             using (var serviceScope = ServiceActivator.GetScope())
             {
                 var dataBase = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
                 if (dataBase == null)
                 {
-                    return View(new List<PostModel>());
+                    return new List<PostModel>();
                 }
+                var user = await dataBase.Users
+                    .Include(u => u.FollowingUsers)
+                    .FirstOrDefaultAsync(u => u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
+                var followedUserIds = user.FollowingUsers.Select(u => u.FollowingUserId).ToList();
                 var posts = await dataBase.Posts
+                    .Where(p => followedUserIds.Contains(p.UserId))
                     .Include(p => p.User)
                     .Include(p => p.Likes)
                     .ThenInclude(l => l.User)
                     .OrderByDescending(p => p.CreatedAt)
                     .ToListAsync();
-                return View(posts);
+                return posts;
+            }
+        }
+
+        public async Task<IEnumerable<PostModel>> GetPopularPosts()
+        {
+            using (var serviceScope = ServiceActivator.GetScope())
+            {
+                var dataBase = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                if (dataBase == null)
+                {
+                    return new List<PostModel>();
+                }
+                var user = await dataBase.Users
+                    .FirstOrDefaultAsync(u => u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
+                var posts = await dataBase.Posts
+                    .Where(x => x.UserId != user.Id)
+                    .Include(p => p.User)
+                    .Include(p => p.Likes)
+                    .ThenInclude(l => l.User)
+                    .OrderByDescending(p => p.CreatedAt)
+                    .ThenByDescending(p => p.Likes.Count())
+                    .ToListAsync();
+                return posts;
             }
         }
     }
--- SimpleSocialMedia/wwwroot/js/feed.js
+++ SimpleSocialMedia/wwwroot/js/feed.js
@@ -1,1 +1,22 @@
-﻿
+﻿$('.tab-link').on('click', function () {
+    $('.tab-link').removeClass('active');
+    $(this).addClass('active');
+
+    var type = $(this).data('type');
+    var method = type == "my-news" ? "GetMyNewsTab" : "GetPopularPostsTab";
+
+    $.ajax({
+        url: `/Feed/${method}`,
+        method: 'GET',
+        success: function (data) {
+            $('.post-feed').html(data);
+        },
+        error: function () {
+            alert('Произошла ошибка при загрузке постов.');
+        }
+    });
+});
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
